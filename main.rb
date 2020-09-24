48f6f51fff1dc0222e3c4aae427a08e2ab9c631c
Carlos Eduardo <ferreiira.kadu@gmail.com>
2020-09-24 11:48:53 -0300

    feat: add robot to get data from api

--- main.rb
+++ main.rb
@@ -1,14 +1,21 @@
+require 'json'
+
 require_relative './robots/input.rb'
+require_relative './robots/starwars.rb'
 
 class Application
     include Input
+    include SWRobot
+
+    attr_accessor :content
 
     def initialize
         name = get_name()
         type = get_type()
 
+        @content = get_info(name, type)
+        puts @content
     end
-
 end
 
 Application.new()
---
+++ robots/starwars.rb
@@ -0,0 +1,67 @@
+require 'open-uri'
+
+module SWRobot
+    def get_url type
+        urls = {
+            "films": "https://swapi.dev/api/films/",
+            "people": "https://swapi.dev/api/people/",
+            "planets": "https://swapi.dev/api/planets/",
+            "species": "https://swapi.dev/api/species/",
+            "starships": "https://swapi.dev/api/starships/",
+            "vehicles": "https://swapi.dev/api/vehicles/"
+        }
+
+        urls[type.to_sym]
+    end
+
+    def get_info name, type
+        data = get_info_by_api name, type
+        dataJSON = JSON.parse(data)
+        raise ArgumentError.new('Name wrong') if dataJSON['count'] == 0
+        # puts dataJSON['results'][0]['name']
+        films_array = get_title_at_all_films dataJSON['results'][0]['films']
+        hw_name = get_homeworld_name dataJSON['results'][0]['homeworld']
+        return {
+            name: name,
+            type: type,
+            full_name: dataJSON['results'][0]['name'],
+            films: films_array,
+            homeworld: hw_name
+        }
+    end
+
+    def get_info_by_api name, type
+        type_url = get_url type
+
+        url_formated = "#{type_url}?search=#{name}"
+
+        puts '> Searching Info...'
+        URI.open(url_formated).read
+    end
+
+    def get_title_at_all_films films_url
+
+        films_title = []
+
+        films_url.each do |url|
+            data_film = URI.open(url).read
+            films_title.push(
+                get_title_by_film data_film
+            )
+        end
+
+        return films_title
+    end
+
+    def get_title_by_film data_film
+        data_film_json = JSON.parse(data_film)
+        return data_film_json['title']
+    end
+
+    def get_homeworld_name homew_url
+        data_hw_json = URI.open(homew_url).read
+        data_hw = JSON.parse data_hw_json
+        
+        data_hw['name']
+    end
+end
